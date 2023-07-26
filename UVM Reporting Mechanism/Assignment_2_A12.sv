4486e71c1faa0bc8836a3203f7b684fbb7a79ea8
Kyrillos Magdi <89218807+CyrilleLeGuerrier@users.noreply.github.com>
2023-07-27 02:04:52 +0300

    Update Assignment_2_A12.sv

--- UVM Reporting Mechanism/Assignment_2_A12.sv
+++ UVM Reporting Mechanism/Assignment_2_A12.sv
@@ -7,7 +7,11 @@ module uvm_tb_top;
   initial 
     begin
       $display("Default Verbosity level : %0d ", uvm_top.get_report_verbosity_level);
+      // get_report_verbosity_level
+      // Gets the verbosity level in effect for this object.  
+      // Reports issued with verbosity greater than this will be filtered out.
       uvm_top.set_report_verbosity_level(UVM_DEBUG);
+      // This method sets the maximum verbosity level for reports for this component.  Any report from this component whose verbosity exceeds this maximum will be ignored.
       $display("Current Verbosity level : %0d ", uvm_top.get_report_verbosity_level);
   	end
   

204c88248678edfed1e25390de47b538af095171
Kyrillos Magdi <89218807+CyrilleLeGuerrier@users.noreply.github.com>
2023-07-27 02:01:43 +0300

    Update Assignment_1_A11.sv

--- UVM Reporting Mechanism/Assignment_1_A11.sv
+++ UVM Reporting Mechanism/Assignment_1_A11.sv
@@ -6,6 +6,9 @@ module uvm_tb_top ();
   
 	initial
     	begin
+	  // UVM_DISPLAY: output to the screen
+	  // The default actions assigned to uvm_info severity. 
+          // UVM_INFO -       UVM_DISPLAY
           `uvm_info("uvm_tb_top_module", "First RTL : Your_System_Name", UVM_NONE); // ID, message, verbosity
     	end
   
